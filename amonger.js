87fdc92bfea1f6bea920ff83fa6bac1ea20e4c27
esfox <osairis100@gmail.com>
2020-09-27 23:18:07 +0800

    Initial commit

---
+++ amonger.js
@@ -0,0 +1,48 @@
+import { MessageEmbed } from 'discord.js';
+
+/** @param {import('discord.js').Message} message */
+export async function amonger(message)
+{
+	if(! message.member.voice.channel)
+		return message.channel.send('You are not in a voice channel. Join a voice channel first.');
+
+	const embed = new MessageEmbed()
+		.setColor('#FFDE2A')
+		.setTitle(`Among Us started in [🔊 ${message.member.voice.channel.name}]`)
+		.setDescription(
+			'Click 🔇 to mute everyone.\n'
+			+ 'Click 🗣️ to unmute everyone.\n'
+			+ 'Click 🛑 if the game has ended.'
+		);
+
+	const control = await message.channel.send({ embed });
+	await Promise.all(
+	[
+		control.react('🔇'),
+		control.react('🗣️'),
+		control.react('🛑'),
+	]);
+
+	const reactions = control.createReactionCollector(
+		({ emoji }, user) =>
+			user.id === message.author.id && [ '🔇', '🗣️', '🛑' ].includes(emoji.name),
+		{ dispose: true, time: 18000000 },
+	);
+
+	const onReact = async ({ emoji }) =>
+	{
+		if(emoji.name === '🛑')
+		{
+			message.delete();
+			return control.delete();
+		}
+
+		const toMute = emoji.name === '🔇';
+		const members = [ ...message.member.voice.channel.members.values() ];
+		for(const member of members)
+			await member.voice.setMute(toMute);
+	};
+
+	reactions.on('collect', onReact);
+	reactions.on('remove', onReact);
+}
---
+++ bot.js
@@ -0,0 +1,16 @@
+import { Client } from 'discord.js';
+import { amonger } from './amonger';
+
+const bot = new Client()
+bot.login(process.env.BOT_TOKEN);
+bot.on('ready', () => console.log(`Connected as ${bot.user.tag}`));
+bot.on('message', message =>
+{
+	if(message.author.bot)
+		return;
+
+	if(message.content !== '!amonger')
+		return;
+
+	amonger(message);
+});
